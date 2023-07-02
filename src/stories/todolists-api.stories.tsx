f883a46d2db20449b89ae318033ab724eadcc6ce
Ivan <fenomen_ny@mail.ru>
2023-07-02 23:55:50 +0300

    lesson 13 part 1

---
+++ src/stories/todolists-api.stories.tsx
@@ -0,0 +1,46 @@
+import axios from 'axios'
+import React, {useEffect, useState} from 'react'
+
+export default {
+    title: 'API'
+}
+
+const settings = {
+    withCredentials: true
+}
+
+export const GetTodolists = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        // здесь мы будем делать запрос и ответ закидывать в стейт.
+        // который в виде строки будем отображать в div-ке
+        axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists', settings)
+            .then((res) => {
+                console.log(res)
+                setState(res.data)
+            })
+    }, [])
+    return <div>{JSON.stringify(state)}</div>
+}
+export const CreateTodolist = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+    }, [])
+
+    return <div>{JSON.stringify(state)}</div>
+}
+export const DeleteTodolist = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+    }, [])
+
+    return <div>{JSON.stringify(state)}</div>
+}
+export const UpdateTodolistTitle = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+    }, [])
+
+    return <div>{JSON.stringify(state)}</div>
+}
+
