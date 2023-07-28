6e89032017983415b3954d6e5c363762bfa9058d
ivanrussui <fenomen_ny@mail.ru>
2023-07-28 16:18:18 +0300

    Snackbar

--- src/app/app-reducer.tsx
+++ src/app/app-reducer.tsx
@@ -1,7 +1,8 @@
 export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
 
 const initialState = {
-    status: 'loading' as RequestStatusType
+    status: 'loading' as RequestStatusType,
+    error: 'Error message 😠' as string | null
 }
 
 type InitialStateType = typeof initialState
@@ -10,6 +11,8 @@ export const appReducer = (state: InitialStateType = initialState, action: Actio
     switch (action.type) {
         case 'APP/SET-STATUS':
             return {...state, status: action.status}
+        case "APP/SET-ERROR":
+            return {...state, error: action.error}
         default:
             return state
     }
@@ -19,6 +22,11 @@ export const setAppStatusAC = (status: RequestStatusType) => ({
     type: 'APP/SET-STATUS', status
 } as const)
 
-export type SetAppStatusActionType = ReturnType<typeof setAppStatusAC>
+export const setAppErrorAC = (error: string | null) => ({
+    type: 'APP/SET-ERROR', error
+} as const)
 
-type ActionsType = SetAppStatusActionType
+export type SetAppStatusActionType = ReturnType<typeof setAppStatusAC>
+export type SetAppErrorActionType = ReturnType<typeof setAppErrorAC>
+
+type ActionsType = SetAppStatusActionType | SetAppErrorActionType
